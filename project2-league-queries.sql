a775f8ed1084168cd2abe35f5f29205bd82ad4d6
holmera1 <robert.a.holmes@vanderbilt.edu>
2020-04-12 22:44:48 -0400

    tables done, stored procedures done and working

--- project2-league-queries.sql
+++ project2-league-queries.sql
@@ -1,142 +1,143 @@
 -- Query to get all distinct patches from the dataset
 SELECT DISTINCT version
-FROM matches;
+FROM matches
+ORDER BY INET_ATON(SUBSTRING_INDEX(CONCAT(version, '0.0'),'.',4)) DESC;
 
 -- Top 5 most played champs of the patch
-SELECT name, COUNT(*), version
-FROM participants
-INNER JOIN champs ON participants.championid = champs.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY name
-ORDER BY COUNT(*) DESC
-LIMIT 5;
+DROP PROCEDURE IF EXISTS getChampsMostPlayed;
+DELIMITER //
+CREATE PROCEDURE getChampsMostPlayed(IN p_version VARCHAR(20))
+BEGIN
+	SELECT name
+	FROM participants
+	INNER JOIN champs ON participants.championid = champs.id
+	LEFT JOIN matches ON participants.matchid = matches.id
+	WHERE version = p_version
+	GROUP BY name
+	ORDER BY COUNT(*) DESC
+	LIMIT 5;
+END //
+
+CALL getChampsMostPlayed('7.10');
 
 -- Top 5 less played champs of the patch
-SELECT name, COUNT(*), version
-FROM participants
-INNER JOIN champs ON participants.championid = champs.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY name
-ORDER BY COUNT(*) ASC
-LIMIT 5;
+DROP PROCEDURE IF EXISTS getChampsLeastPlayed;
+DELIMITER //
+CREATE PROCEDURE getChampsLeastPlayed(IN p_version VARCHAR(20))
+BEGIN
+	SELECT name
+	FROM participants
+	INNER JOIN champs ON participants.championid = champs.id
+	LEFT JOIN matches ON participants.matchid = matches.id
+	WHERE version = p_version
+	GROUP BY name
+	ORDER BY COUNT(*) ASC
+	LIMIT 5;
+END //
+
+CALL getChampsLeastPlayed('7.9');
 
 -- Top 5 most banned champs of the patch
-SELECT name, COUNT(*), version
-FROM teambans
-INNER JOIN champs ON teambans.championid = champs.id
-LEFT JOIN matches ON teambans.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY name
-ORDER BY COUNT(*) DESC
-LIMIT 5;
+DROP PROCEDURE IF EXISTS getChampsMostBanned;
+DELIMITER //
+CREATE PROCEDURE getChampsMostBanned(IN p_version VARCHAR(20))
+BEGIN
+	SELECT name
+	FROM teambans
+	INNER JOIN champs ON teambans.championid = champs.id
+	LEFT JOIN matches ON teambans.matchid = matches.id
+	WHERE version = p_version
+	GROUP BY name
+	ORDER BY COUNT(*) DESC
+	LIMIT 5;
+END //
+
+CALL getChampsMostBanned('7.10');
 
 -- Top 5 least banned champs of the patch
-SELECT name, COUNT(*), version
-FROM teambans
-INNER JOIN champs ON teambans.championid = champs.id
-LEFT JOIN matches ON teambans.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY name
-ORDER BY COUNT(*) ASC
-LIMIT 5;
+DROP PROCEDURE IF EXISTS getChampsLeastBanned;
+DELIMITER //
+CREATE PROCEDURE getChampsLeastBanned(IN p_version VARCHAR(20))
+BEGIN
+	SELECT name
+	FROM teambans
+	INNER JOIN champs ON teambans.championid = champs.id
+	LEFT JOIN matches ON teambans.matchid = matches.id
+	WHERE version = p_version
+	GROUP BY name
+	ORDER BY COUNT(*) ASC
+	LIMIT 5;
+END //
+
+CALL getChampsLeastBanned('7.10');
 
 -- Highest win rate champs of the patch
-SELECT name, SUM(win), COUNT(*), TRUNCATE(((SUM(win)/COUNT(*))*100), 2) AS winrate
-FROM participants
-INNER JOIN champs ON participants.championid = champs.id
-INNER JOIN stats ON participants.id = stats.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY name
-ORDER BY (SUM(win)/COUNT(*)) DESC;
+DROP PROCEDURE IF EXISTS getChampsBestWinrate;
+DELIMITER //
+CREATE PROCEDURE getChampsBestWinrate(IN p_version VARCHAR(20))
+BEGIN
+	SELECT name, TRUNCATE(((SUM(win)/COUNT(*))*100), 2) AS winrate
+	FROM participants
+	INNER JOIN champs ON participants.championid = champs.id
+	INNER JOIN stats ON participants.id = stats.id
+	LEFT JOIN matches ON participants.matchid = matches.id
+	WHERE version = p_version
+	GROUP BY name
+	ORDER BY (SUM(win)/COUNT(*)) DESC
+    LIMIT 5;
+END //
+
+CALL getChampsBestWinrate('7.10');
 
 -- Lowest win rate champs of the patch
-SELECT name, SUM(win), COUNT(*), TRUNCATE(((SUM(win)/COUNT(*))*100), 2) AS winrate
-FROM participants
-INNER JOIN champs ON participants.championid = champs.id
-INNER JOIN stats ON participants.id = stats.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY name
-ORDER BY SUM(win) ASC;
+DROP PROCEDURE IF EXISTS getChampsWorstWinrate;
+DELIMITER //
+CREATE PROCEDURE getChampsWorstWinrate(IN p_version VARCHAR(20))
+BEGIN
+	SELECT name, TRUNCATE(((SUM(win)/COUNT(*))*100), 2) AS winrate
+	FROM participants
+	INNER JOIN champs ON participants.championid = champs.id
+	INNER JOIN stats ON participants.id = stats.id
+	LEFT JOIN matches ON participants.matchid = matches.id
+	WHERE version = p_version
+	GROUP BY name
+	ORDER BY (SUM(win)/COUNT(*)) ASC
+    LIMIT 5;
+END //
+
+CALL getChampsWorstWinrate('7.10');
 
 -- Most used items of the patch
-SELECT item1, COUNT(*)
-FROM stats
-INNER JOIN participants ON stats.id = participants.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY item1
-ORDER BY COUNT(*) DESC;
+DROP VIEW IF EXISTS popularItems;
+CREATE VIEW popularItems AS
+SELECT item, COUNT(*) AS purchases
+FROM (
+	SELECT item1 AS item FROM stats
+	UNION ALL
+	SELECT item2 FROM stats
+	UNION ALL
+	SELECT item3 FROM stats
+	UNION ALL
+	SELECT item4 FROM stats
+	UNION ALL
+	SELECT item5 FROM stats
+	UNION ALL
+    SELECT item6 FROM stats
+) d
+GROUP BY item;
 
-SELECT item2, COUNT(*)
-FROM stats
-INNER JOIN participants ON stats.id = participants.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY item2
-ORDER BY COUNT(*) DESC;
+DROP PROCEDURE IF EXISTS getMostPopularItems;
+DELIMITER //
+CREATE PROCEDURE getMostPopularItems(IN p_version VARCHAR(20))
+BEGIN
+	SELECT item, name, purchases
+	FROM popularItems
+	INNER JOIN items ON popularItems.item = items.id
+	ORDER BY purchases DESC
+	LIMIT 5;
+END //
 
-SELECT item3, COUNT(*)
-FROM stats
-INNER JOIN participants ON stats.id = participants.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY item3
-ORDER BY COUNT(*) DESC;
-
-SELECT item4, COUNT(*)
-FROM stats
-INNER JOIN participants ON stats.id = participants.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY item4
-ORDER BY COUNT(*) DESC;
-
-SELECT item5, COUNT(*)
-FROM stats
-INNER JOIN participants ON stats.id = participants.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY item5
-ORDER BY COUNT(*) DESC;
-
-SELECT item6, COUNT(*)
-FROM stats
-INNER JOIN participants ON stats.id = participants.id
-LEFT JOIN matches ON participants.matchid = matches.id
-WHERE version = '\"7.10.187.9675\"'
-GROUP BY item6
-ORDER BY COUNT(*) DESC;
-
-
-
-SELECT items1.item, COUNT(*)
-FROM
-(	
-	SELECT item1 AS item
-	FROM stats
-	INNER JOIN participants ON stats.id = participants.id
-	LEFT JOIN matches ON participants.matchid = matches.id
-	WHERE version = '\"7.10.187.9675\"'
-	GROUP BY item1
-	ORDER BY COUNT(*) DESC
-) AS items1 
-INNER JOIN 
-(
-	SELECT item2 AS item, 
-	FROM stats
-	INNER JOIN participants ON stats.id = participants.id
-	LEFT JOIN matches ON participants.matchid = matches.id
-	WHERE version = '\"7.10.187.9675\"'
-	GROUP BY item2
-	ORDER BY COUNT(*) DESC
-) AS items2 ON items1.item = items2.item
-
-GROUP BY item
-ORDER BY COUNT(*) DESC;
+CALL getMostPopularItems('7.10');
 
 
 
